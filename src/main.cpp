217d20349e1360e5dc0affbcbc5838d2133eaa3a
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-10-03 17:24:37 -0700

    vestigial

--- src/main.cpp
+++ src/main.cpp
@@ -45,13 +45,10 @@ Real split_ratio = 0.5; /* split hubs distance ratio */
 Real switch_percent = 100.0; /* switch to ghost hub/tree task parallelism when we reach this percentage of leaves */
 Index min_hub_size = 10; /* hubs below this size automatically become all leaves */
 
-bool level_synch = true; /* level synchronous construction */
 bool verify_tree = false; /* verify correctness of constructed cover tree */
 bool verify_graph = false; /* verify correctness of constructed epsilon neighbor graph (slow because it builds brute force graph to compare) */
-bool verify_graph_plus = false; /* count number of wrong edges found */
 bool build_graph = false;
 bool verbose = false;
-int nthreads = 1;
 
 void parse_arguments(int argc, char *argv[]);
 bool graph_is_correct(const PointVector& points, Real radius, const std::vector<IndexVector>& graph);
@@ -97,15 +94,7 @@ int main(int argc, char *argv[])
 
         fmt::print("[msg::{},time={:.3f}] constructed epsilon graph [vertices={},edges={},avg_deg={:.3f}]\n", __func__, t, size, num_edges, (num_edges+0.0)/size);
 
-        if (verify_graph_plus)
-        {
-            t = -omp_get_wtime();
-            const auto& [missing, total] = count_missing(points, radius, graph);
-            t += omp_get_wtime();
-
-            fmt::print("[msg::{},time={:.3f}] {:.3f} percent of edges missing [num_missing={},num_total={}]\n",  __func__, t, (100.*missing)/total, missing, total);
-        }
-        else if (verify_graph)
+        if (verify_graph)
         {
             t = -omp_get_wtime();
             bool correct = graph_is_correct(points, radius, graph);
@@ -127,28 +116,22 @@ void parse_arguments(int argc, char *argv[])
         fprintf(stderr, "         -S FLOAT  hub split ratio [%.2f]\n", split_ratio);
         fprintf(stderr, "         -s FLOAT  switch percent [%.2f]\n", switch_percent);
         fprintf(stderr, "         -l INT    minimum hub size [%lu]\n", (size_t)min_hub_size);
-        fprintf(stderr, "         -t INT    number of threads [%d]\n", nthreads);
-        fprintf(stderr, "         -A        asynchronous tree construction\n");
         fprintf(stderr, "         -T        verify tree correctness\n");
         fprintf(stderr, "         -G        verify graph correctness [assumes -r]\n");
-        fprintf(stderr, "         -C        count number of wrong edges [assumes -r]\n");
         fprintf(stderr, "         -v        verbose\n");
         fprintf(stderr, "         -h        help message\n");
         exit(err);
     };
 
     int c;
-    while ((c = getopt(argc, argv, "r:S:s:Ct:e:l:TAGvh")) >= 0)
+    while ((c = getopt(argc, argv, "r:S:s:e:l:TGvh")) >= 0)
     {
         if      (c == 'r') radius = atof(optarg);
         else if (c == 'S') split_ratio = atof(optarg);
         else if (c == 's') switch_percent = atof(optarg);
         else if (c == 'l') min_hub_size = atoi(optarg);
-        else if (c == 't') nthreads = atoi(optarg);
-        else if (c == 'A') level_synch = false;
         else if (c == 'T') verify_tree = true;
         else if (c == 'G') verify_graph = true;
-        else if (c == 'C') verify_graph_plus = true;
         else if (c == 'v') verbose = true;
         else if (c == 'h') usage(0);
     }
@@ -162,7 +145,7 @@ void parse_arguments(int argc, char *argv[])
     fname = argv[optind];
     build_graph = (radius > 0);
 
-    omp_set_num_threads(nthreads);
+    int nthreads;
 
     #pragma omp parallel
     nthreads = omp_get_num_threads();
@@ -171,7 +154,7 @@ void parse_arguments(int argc, char *argv[])
     for (int i = 0; i < argc-1; ++i) ss << argv[i] << " "; ss << argv[argc-1];
     fmt::print("[msg::{}] cmd: {} [omp_num_threads={},commit={},when='{}']\n", __func__, ss.str(), nthreads, GIT_COMMIT, return_current_date_and_time());
     fmt::print("[msg::{}] point parameters: [file='{}',dim={},fp={}]\n", __func__, fname, DIM_SIZE, sizeof(Real)<<3);
-    fmt::print("[msg::{}] ctree parameters: [split_ratio={:.2f},switch_percent={:.2f},min_hub_size={},level_synch={},verify_tree={},verbose={}]\n", __func__, split_ratio, switch_percent, min_hub_size, level_synch, verify_tree, verbose);
+    fmt::print("[msg::{}] ctree parameters: [split_ratio={:.2f},switch_percent={:.2f},min_hub_size={},verify_tree={},verbose={}]\n", __func__, split_ratio, switch_percent, min_hub_size, verify_tree, verbose);
     if (build_graph) fmt::print("[msg::{}] graph parameters: [radius={:.3f},verify_graph={}]\n", __func__, radius, verify_graph);
 }
 
