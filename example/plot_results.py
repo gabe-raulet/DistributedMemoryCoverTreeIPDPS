ed7dfd31cd92d10de9834d6e0397bbef500b1bc1
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-10-07 13:59:08 -0700

    good

--- example/plot_results.py
+++ example/plot_results.py
@@ -2,10 +2,10 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 from collections import namedtuple
-
-#  iter_cum_times = np.add.accumulate(iter_times)
-
-experiments = json.load(open("results.json", "r"))["experiments"]
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+from collections import namedtuple
 
 BuildInfo = namedtuple("BuildInfo", ["total_time", "num_hubs", "num_leaves", "num_levels", "num_vertices", "iter_times"])
 GraphInfo = namedtuple("GraphInfo", ["total_time", "num_edges", "num_vertices", "dist_comps", "thread_edges", "thread_queries"])
@@ -39,8 +39,42 @@ def parse_parameter_info(parameter_info):
 def parse_points_info(points_info):
     return PointsInfo(dimension=points_info["dimension"], float_size=points_info["float_size"], filename=points_info["filename"], size=points_info["size"])
 
-for exp in experiments:
+def get_points_info_string(pi):
+    return f"{pi.dimension}d.{pi.float_size}fp.{pi.size}size"
+
+def get_params_info_string(pi):
+    return f"{pi.min_hub_size}l.{pi.num_threads}t.{pi.radius:.2f}r"
+
+def plot_build_iter_info(exp):
     build_info = parse_build_info(exp["build_info"])
     graph_info = parse_graph_info(exp["graph_info"])
     parameter_info = parse_parameter_info(exp["parameter_info"])
     points_info = parse_points_info(exp["points_info"])
+
+    fig, axs = plt.subplots(3)
+
+    cumulative_times = np.add.accumulate(build_info.iter_times)
+    xdata = np.array(range(len(build_info.num_hubs)))
+    avg_hub_sizes = (points_info.size - build_info.num_leaves)[:-1] / (build_info.num_hubs)[:-1]
+
+    axs[0].plot(xdata, build_info.num_leaves, ':x', linewidth=0.7, color='green', label="# of leaves")
+    axs[0].plot(xdata, build_info.num_vertices, '--o', markersize=2.5, linewidth=1.0, color='blue', label="# of vertices")
+    axs[0].legend()
+
+    axs[1].plot(xdata, build_info.num_levels, ':s', color='red', label="# of levels")
+    axs[1].plot(xdata[:-1], build_info.num_hubs[:-1], '-^', linewidth=0.5, color='black', label="# of hubs")
+    axs[1].plot(xdata[:-1], avg_hub_sizes, ':*', color='orange', label="average hub size")
+    axs[1].set_yscale("log")
+    axs[1].legend()
+
+    axs[2].plot(xdata, build_info.iter_times, ':.', linewidth=0.5, color='darkgreen', label="iteration runtime")
+    axs[2].plot(xdata, cumulative_times, '--x', linewidth=0.5, color='purple', label="cumulative runtime")
+    axs[2].set_ylabel("Seconds")
+    axs[2].legend()
+
+    plt.show()
+    fig.savefig(f"build_iter_info.{get_points_info_string(points_info)}.{get_params_info_string(parameter_info)}.pdf")
+
+experiments = json.load(open("results.json", "r"))["experiments"]
+
+for exp in experiments: plot_build_iter_info(exp)
--- example/test.sh
+++ example/test.sh
@@ -3,18 +3,18 @@
 # export OMP_PROC_BIND=spread
 # export OMP_PLACES=threads
 
-export FILENAME=../../points
-export RADIUS=2.0
-export SPLIT_RATIO=0.8
+export FILENAME=../points
+export RADIUS=0.05
+export SPLIT_RATIO=0.5
 
 counter=1
 
-for THREAD_COUNT in 1 2 3 4 5 6 7 8 9 10 11 12
+for THREAD_COUNT in 1 2 4 8
 do
     export OMP_NUM_THREADS=$THREAD_COUNT
     export FNAME= ${COUNTER}
 
-    ../../main -r $RADIUS -S $SPLIT_RATIO -o out.${counter}.json $FILENAME
+    ../main -r $RADIUS -S $SPLIT_RATIO -o out.${counter}.json $FILENAME
     counter=$((counter+1))
 done
 
