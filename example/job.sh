65a58418a2c2c519fedb5a19a9f98b72806c0568
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-10-07 11:12:47 -0700

    add this

---
+++ example/job.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH -N 1
+#SBATCH -C cpu
+#SBATCH -q debug
+#SBATCH -o output.txt
+#SBATCH --mail-user=ghraulet@lbl.gov
+#SBATCH --mail-type=ALL
+#SBATCH -t 30
+
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+
+export FILENAME=points
+export RADIUS=2.0
+export SPLIT_RATIO=0.8
+
+for THREAD_COUNT in 1 2 4 8 16 32 64 128
+do
+    export OMP_NUM_THREADS=$(THREAD_COUNT)
+    export OUTPUT=log.$(THREAD_COUNT)threads.$(RADIUS)radius.$(SPLIT_RATIO)split_ratio.json
+
+    numactl -i0-7 ./main -r $(RADIUS) -S $(SPLIT_RATIO) -o $(OUTPUT) $(FILENAME)
+done
